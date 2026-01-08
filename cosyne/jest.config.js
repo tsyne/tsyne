6155ae60a6028560f0808d63fe86088f4d820573
Paul Hammant <paul@hammant.org>
2026-01-08 13:11:49 +0000

    ci should not skip cosyne tests

---
+++ cosyne/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/test/**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts'
+  ]
+};
